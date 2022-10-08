b0602a1b1b224ce0c961cc9f039c0259049cf36d
sfcc5555 <sfcc.5555@gmail.com>
2022-10-08 14:32:39 -0500

    Ver. 1.3

--- portafolio.js
+++ portafolio.js
@@ -1,6 +1,7 @@
 let m=document.getElementById("menu");
 let ca=document.querySelector(".containerArchitecture");
-let cce=document.querySelector(".containerCivilEngineering")
+let cce=document.querySelector(".containerCivilEngineering");
+let body=document.querySelector("body");
 
 function mostrarMenu() {
     m.classList.toggle("menuContainer")
@@ -13,11 +14,27 @@ const architectureList =[];
 architectureList.push({
     image: "./IMAGENES/viviendaDoctorVerdugoPlantas.png",
     name: "Vivienda Doctor Verdugo",
+    content: [{image:"./IMAGENES/viviendaDoctorVerdugoPlantas.png",
+                name:"Plano Plantas",
+                content:"./IMAGENES/viviendaDoctorVerdugoPlantas.pdf",
+            },
+            {image:"./IMAGENES/viviendaDoctorVerdugoElevaciones.png",
+                name:"Plano Elevaciones",
+                content:"./IMAGENES/viviendaDoctorVerdugoPlantas.pdf",
+            }],
 });
 
 architectureList.push({
     image: "./IMAGENES/viviendaJorgeBenavidesPlantas.png",
     name: "Vivienda Jorge Benavides",
+    content: [{image:"./IMAGENES/viviendaJorgeBenavidesPlantas.png",
+                name:"Plano Plantas",
+                content:"./IMAGENES/viviendaJorgeBenavidesPlantas.pdf",
+            },
+            {image:"./IMAGENES/viviendaJorgeBenavidesElevaciones.png",
+                name:"Plano Elevaciones",
+                content:"./IMAGENES/viviendaJorgeBenavidesElevaciones.pdf",
+            }],
 });
 
 
@@ -28,9 +45,8 @@ const civilEngineeringList =[];
 civilEngineeringList.push({
     image: "./IMAGENES/murosDeContencionSeñorMacancela.png",
     contenido:"./IMAGENES/murosDeContencionSeñorMacancela.pdf",
-    name: "Bonded",
+    name: "Muros de Contención - Vivienda Señor Macancela",
 });
-
 
 
 /*<div>
@@ -43,22 +59,64 @@ civilEngineeringList.push({
 
 function renderArchitecture()
 {
-
+    positionCounter=0
     for (architecture of architectureList) {
-        contenedorArchitecture=document.createElement("div");
+        
+        positionCounter++
 
-        linkArchitecture=document.createElement("a");
-        linkArchitecture.setAttribute("href",architecture.image);
-        linkArchitecture.setAttribute("target","_BLANK");
+        contenedorArchitecture=document.createElement("div");
 
         imagenArchitecture=document.createElement("img");
         imagenArchitecture.setAttribute("src",architecture.image);
         imagenArchitecture.setAttribute("alt",architecture.name);
         imagenArchitecture.setAttribute("width","75%");
+        imagenArchitecture.setAttribute("position",positionCounter + "A");
+
+        nombreArchitecture=document.createElement("p");
+        nombreArchitecture.setAttribute("class","projectName")
+        nombreArchitecture.innerText=architecture.name;
         
         ca.appendChild(contenedorArchitecture);
-        contenedorArchitecture.appendChild(linkArchitecture);
-        linkArchitecture.appendChild(imagenArchitecture);
+        contenedorArchitecture.appendChild(imagenArchitecture);
+        contenedorArchitecture.appendChild(nombreArchitecture);
+
+
+        p=document.createElement("section");
+        p.setAttribute("class","inactive");
+        p.setAttribute("id",positionCounter + "A")
+        
+        c=document.createElement("div");
+        c.setAttribute("class","close");
+        c.innerText="x";
+        p.appendChild(c);
+
+        body.appendChild(p);
+
+        for (architectureProject of architecture.content) {
+
+
+            
+            contenedorArchitectureProject=document.createElement("div");
+
+            imagenArchitectureProject=document.createElement("img");
+            imagenArchitectureProject.setAttribute("src",architectureProject.image);
+            imagenArchitectureProject.setAttribute("alt",architectureProject.name);
+            imagenArchitectureProject.setAttribute("width","75%");
+    
+            nombreArchitectureProject=document.createElement("p");
+            nombreArchitectureProject.setAttribute("class","projectName")
+            nombreArchitectureProject.innerText=architectureProject.name;
+            
+            p.appendChild(contenedorArchitectureProject);
+            contenedorArchitectureProject.appendChild(imagenArchitectureProject);
+            contenedorArchitectureProject.appendChild(nombreArchitectureProject);
+    
+        }
+
+        imagenArchitecture.addEventListener("click",renderProject);
+        c.addEventListener("click",closeProject);
+        
+
     }
 
 }
@@ -67,22 +125,21 @@ function renderCivilEngineering()
 {
 
     for (civilEngineering of civilEngineeringList) {
+        
         contenedorCivilEngineering=document.createElement("div");
-
-        linkCivilEngineering=document.createElement("a");
-        linkCivilEngineering.setAttribute("href",civilEngineering.contenido);
-        linkCivilEngineering.setAttribute("target","_BLANK");
 
         imagenCivilEngineering=document.createElement("img");
         imagenCivilEngineering.setAttribute("src",civilEngineering.image);
         imagenCivilEngineering.setAttribute("alt",civilEngineering.name);
-        imagenCivilEngineering.setAttribute("width","75%")
-        
+        imagenCivilEngineering.setAttribute("width","75%");
+
+        nombreCivilEngineering=document.createElement("p");
+        nombreCivilEngineering.setAttribute("class","projectName")
+        nombreCivilEngineering.innerText=civilEngineering.name;
     
         cce.appendChild(contenedorCivilEngineering);
-        contenedorCivilEngineering.appendChild(linkCivilEngineering);
-        linkCivilEngineering.appendChild(imagenCivilEngineering);
-
+        contenedorCivilEngineering.appendChild(imagenCivilEngineering);
+        contenedorCivilEngineering.appendChild(nombreCivilEngineering);
     }
 
 }
@@ -90,3 +147,20 @@ function renderCivilEngineering()
 renderArchitecture();
 
 renderCivilEngineering();
+
+function renderProject(evento) {
+    
+    eventPosition = evento.target.attributes.position.value;
+    
+    projectToRender = document.getElementById(eventPosition)
+    
+    projectToRender.classList.toggle("projectContainer");
+
+
+}
+
+function closeProject() {
+
+    projectToRender.classList.toggle("projectContainer");
+
+}
